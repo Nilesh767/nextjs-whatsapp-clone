dc3c32e6b28d094e00a1032f17213106c28ded8d
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-04-15 16:37:04 +0530

    changes in loading component

--- Components/Loading.js
+++ Components/Loading.js
@@ -1,27 +1,48 @@
+import styled from "styled-components";
 import Loader from "react-loader-spinner";
+
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const Loading = () => {
   return (
-    <center
-      style={{
-        display: "grid",
-        placeItems: "center",
-        height: "100vh",
-        backgroundColor: "#e3dfde",
-      }}
-    >
-      <div>
-        <img
-          src="/whatsappLogo.svg"
-          alt="whatsapp logo"
-          height={350}
-          style={{ marginBottom: 20 }}
-        />
+    <Container>
+      <LoadingContainer>
+        <Logo src="/whatsappLogo.svg" alt="whatsapp logo" />
         <Loader type="Circles" color="#075E54" height={80} width={80} />
-      </div>
-    </center>
+      </LoadingContainer>
+    </Container>
   );
 };
 
 export default Loading;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-items: center;
+  justify-content: center;
+  background-color: whitesmoke;
+`;
+
+const LoadingContainer = styled.div`
+  padding: 100px;
+  display: flex;
+  border-radius: 5px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: white;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+  @media only screen and (max-width: 600px) {
+    padding: 50px;
+  }
+`;
+
+const Logo = styled.img`
+  height: 250px;
+  margin-bottom: 20px;
+  @media only screen and (max-width: 600px) {
+    height: 150px;
+  }
+`;
